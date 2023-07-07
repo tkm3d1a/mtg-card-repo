ac9a35775b10c9abe7f665d08255e848c0d1c28a
Tim Klimpel <tim.klimpel@gmail.com>
2023-07-07 15:55:26 -0400

    init intelliJ project

---
+++ backend/card-testing/src/main/java/com/tkm3d1a/cardtesting/CardTestingApplication.java
@@ -0,0 +1,13 @@
+package com.tkm3d1a.cardtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardTestingApplication.class, args);
+    }
+
+}
---
+++ backend/card-testing/src/test/java/com/tkm3d1a/cardtesting/CardTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkm3d1a.cardtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
